045b9cee5fcafa3c71b053f7df294555060bd0d3
Swagytor <swagytor@yandex.ru>
2023-07-09 11:47:04 +0300

    Homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) UNIQUE NOT NULL,
+	company_name text,
+	contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(50)
+);
+
+SELECT * FROM orders;
+SELECT * FROM customers;
+SELECT * FROM employees;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os.path
+import psycopg2
+
+BASEDIR = os.path.abspath('..')
+PATH_TO_DATA = os.path.join(BASEDIR, 'homework-1', 'north_data')
+
+
+def get_csv_data(path):
+    with open(path, 'r', encoding='utf-8') as csv_file:
+        csv_data = [data for data in csv.reader(csv_file)]
+
+    return csv_data
+
+
+def add_data(table_name, data, cur):
+    column_amount = ', '.join(["%s"] * len(data[0]))
+
+    cur.executemany(f'INSERT INTO {table_name} VALUES({column_amount})', data[1:])
+
+
+if __name__ == '__main__':
+    customers_data = get_csv_data(os.path.join(PATH_TO_DATA, 'customers_data.csv'))
+    employee_data = get_csv_data(os.path.join(PATH_TO_DATA, 'employees_data.csv'))
+    orders_data = get_csv_data(os.path.join(PATH_TO_DATA, 'orders_data.csv'))
+
+    with psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="12345"
+    ) as conn:
+        with conn.cursor() as cur:
+
+            add_data('customers', customers_data, cur)
+            add_data('employees', employee_data, cur)
+            add_data('orders', orders_data, cur)
+
+    conn.close()
